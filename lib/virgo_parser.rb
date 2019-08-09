a95019f5be2648b38b6d2d9b690a4dc69d68b544
Nestor Walker <naw4t@virginia.edu>
2019-08-09 13:29:37 -0400

    slash on classpath

--- lib/virgo_parser.rb
+++ lib/virgo_parser.rb
@@ -1,7 +1,7 @@
 module VirgoParser
   require 'virgo_parser/eds'
   require 'java'
-  $CLASSPATH << File.expand_path('./lib/java')
+  $CLASSPATH << File.expand_path('./lib/java/')
   puts $CLASSPATH
   java_import 'edu.virginia.virgo.Main'
 end
