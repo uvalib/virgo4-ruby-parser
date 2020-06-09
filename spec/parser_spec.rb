efb54faad65661bc5735a3d14b73ca41a4a89787
Nestor Walker <naw4t@virginia.edu>
2020-06-09 09:41:11 -0400

    VIRGONEW-1345 Updated parser

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe VirgoParser::EDS do
   it 'works' do
     parsed_string = VirgoParser::EDS.parse "title:{bananas}"
-    expect(parsed_string).to eq("(TI bananas)")
+    expect(parsed_string).to eq({"query-0"=>"TI:bananas"})
   end
 end
